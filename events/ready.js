af6f39d185168389518fa2c33e833603aa28a656
nize <nize@puersa.com>
2021-10-04 11:41:18 +0800

    set activity message prefix to use config setting

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Event = require("../structures/event.js");
 
 module.exports = new Event("ready", client => {
 	console.log("--------- Noize is ready! ---------");
-	client.user.setActivity('-help', { type: 'LISTENING' });
+	client.user.setActivity(`${client.prefix}help`, { type: 'LISTENING' });
 });

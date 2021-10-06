7b5fa6abc607a9c322f48fd1b961031988df70f9
nize <nize@puersa.com>
2021-10-06 19:47:34 +0800

    remove unnecessary check

--- commands/seek.js
+++ commands/seek.js
@@ -7,8 +7,7 @@ module.exports = new Command({
 	permission: "SEND_MESSAGES",
 	async run(message, args, client) {
         const queue = client.player.getQueue(message.guild);
-        if (!queue || !args[1]) return;
-		if(!args[1] < 0) return;
+        if (!queue || !queue.playing || !args[1]) return;
         if (args[1] * 1000 >= queue.current.durationMS) return message.react('❌');
 		await queue.seek(args[1] * 1000);
 

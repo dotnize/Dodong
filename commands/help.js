2d28acc128417a8a490164791ceba43b22331827
Nathaniel Tampus <hello@nize.ph>
2022-02-16 15:55:34 +0800

    split help command list to multiple pages

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,43 @@
 const Command = require("../structures/command.js");
 const { MessageEmbed } = require('discord.js');
+const generatePages = require('../utils/embedPages.js');
 module.exports = new Command({
 	name: "help",
 	aliases: [],
 	description: "Displays all server commands",
 	permission: "SEND_MESSAGES",
 	async run(message, args, client, slash) {
-		let list = "Commands:\n";
-		client.commands.forEach(cmd => {
-			list = list.concat(`\n${client.prefix}${cmd.name}`);
-			if(cmd.options.length > 0) {
-				cmd.options.forEach(option => {
-					list = list.concat(` <${option.name}>`);
-				});
-			}
-			//list = list.concat(`\n${cmd.description}\n`);
-			// todo: add description, split to multiple pages, add buttons for pages
-		});
-        const embed = new MessageEmbed();
-        embed.setColor('#44b868');
-        embed.setDescription(list);
-        return message.reply({ embeds: [embed] });
+		const pages = [];
+        let page = 1, emptypage = false, commandInfo;
+        do {
+            const pageStart = 6 * (page - 1);
+            const pageEnd = pageStart + 6;
+            const commands = client.commands.slice(pageStart, pageEnd).map(cmd => {
+				commandInfo = `**${client.prefix}${cmd.name}**`;
+				if(cmd.options.length > 0) {
+					cmd.options.forEach(option => {
+						commandInfo = commandInfo.concat(` <${option.name}>`);
+					});
+				}
+				commandInfo = commandInfo.concat(`\n${cmd.description}\n`);
+				return commandInfo;
+			});
+            if(commands.length) {
+				const embed = new MessageEmbed();
+				embed.setAuthor({ name: `Commands` });
+                embed.setDescription(`${commands.join('\n')}`);
+                embed.setColor(page%2 ? '#44b868' : '#b84e44');
+                pages.push(embed);
+                page++;
+            }
+            else {
+                emptypage = true;
+                if(page === 2) {
+                    return message.reply({ embeds: [pages[0]] });
+                }
+            }
+        } while(!emptypage);
+
+		generatePages(message, pages, { timeout: 40000 });
 	}
 });

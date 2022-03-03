c1e73d6e2b2b5c6888017650a90920d468beb5cb
Nathaniel Tampus <hello@nize.ph>
2022-03-03 14:08:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,13 @@ module.exports = {
 
 	//	These values will be ignored if you have set the environment variables (must be in uppercase)
 	
-		prefix: "-",
-		botToken: "BOT TOKEN HERE",
-		clientId: "", // the bot's client ID, leave empty to disable slash commands
-		geniusApiToken: "GENIUS.COM CLIENT ACCESS TOKEN HERE", // optional - https://genius.com/api-clients
+		prefix: "-", // required, command prefix
+		botToken: "", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+		clientId: "", // optional, the bot's client ID, leave empty to disable slash commands
+		geniusApiToken: "", // optional, but recommended for lyrics search - https://genius.com/api-clients
+
+		// https://github.com/JoshCunningHum/Dodong-webplayer
+		// still under development, so you should leave these empty
 		webplayer: "", // optional
 		cors: "*", // optional - stored in an array for multiple socket connections in the future. Set to "*" to accept all
 };

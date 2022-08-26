3ae7abb58e61106b81c5f8f220bd9602790fe9c9
Nathaniel Tampus <contact@nize.ph>
2022-08-26 17:21:56 +0800

    remove token
    oopsie

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
 	//	These values will be ignored if you have set the environment variables (must be in uppercase)
 	
 		prefix: "-", // required, command prefix
-		botToken: "ODM0Njk5OTYwMzQ3NTI1MTcw.GTu3Mk.6kItKIKPdfiuPenSncdb6q9jl3WkUTJSDARISI", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
-		clientId: "834699960347525170", // optional, the bot's client ID, leave empty to disable slash commands
+		botToken: "", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+		clientId: "", // optional, the bot's client ID, leave empty to disable slash commands
 		geniusApiToken: "", // optional, but recommended for lyrics search - https://genius.com/api-clients
 
 		// https://github.com/JoshCunningHum/Dodong-webplayer
